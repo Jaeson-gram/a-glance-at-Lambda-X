5a47d9d1d5e1c3dfde438b7ae0fefb9ff00f569b
Je <54561597+Jaeson-gram@users.noreply.github.com>
2024-03-06 07:50:48 -0800

    Update Main.java
    Working with a custom Functional Interface

--- com/learningJava/Main.java
+++ com/learningJava/Main.java
@@ -30,9 +30,31 @@ public class Main {
             // we also are not allowed to use variable names outside the Lambda X's code blocks as passed parameters in the Lambda X
             // list.forEach(nato) -> { ..... will not compile
 
-
         });
+        System.out.println();
+        int result = calculator((Integer a, Integer b) -> a + b, 16, 7);
+        // Lambda Expression can be replaced with method reference -> int result = calculator(Integer::sum, 16, 7);
+
+        // int result = calculator((var a, Integer b) -> a + b, 16, 7); -> Cannot mix 'var' and explicitly typed parameters in lambda expression
+
+        var result2 = calculator(( a, b) -> a / b, 16.0, 7.0);
+        var result3 = calculator((a, b) -> a.toUpperCase() + " " + b.toUpperCase(), "Ayomide", "OreOluwa");
 
 
+//        var result4 = calculator(( a, b) -> { var c = a / b;
+//            return c;
+//        }, 16.0, 7.0);
+        //            -> return statements only used in codeblocks, and when the method has a return type defined for it, you've got to have that return statement in your code-block
+
+
+
+    }
+
+    public static <T> T calculator(Operation<T> function, T value1, T value2){
+
+        T result = function.operate(value1,value2);
+        System.out.println("Result of operation: " + result);
+
+        return result;
     }
 }
